114bea5f354c4a6584638c136373c1e0cb35b89f
Rashiduulu <nv.nurtilek@gmail.com>
2024-02-06 19:25:27 +0600

    create user modal

---
+++ api/modals/UserModal.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      uniquee: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      uniquee: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userScheme);
+
+export default User;
