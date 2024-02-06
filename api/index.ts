d881f179d51d8a0c78029ca55201948ed144be8f
Rashiduulu <nv.nurtilek@gmail.com>
2024-02-06 16:41:08 +0600

    connect to database

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,16 @@
 import express from "express";
 
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017/house-project")
+  .then(() => {
+    console.log("Connected to MD");
+  })
+  .catch((error: Error) => {
+    console.log("Error connecting to the database: " + error);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
